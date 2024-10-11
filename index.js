73ecd0566cfc2c1792725058f6f86aa4201cabb1
saurabhpushpam <145956198+saurabhpushpam@users.noreply.github.com>
2024-10-11 14:55:00 +0530

    commit

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-  res.send(`document.addEventListener("DOMContentLoaded", function() {
-  if (window.location.pathname.startsWith('/products/')) {
-    console.log("This is a product page.");
-    alert("Product Page);
+  //   res.send(`document.addEventListener("DOMContentLoaded", function() {
+  //   if (window.location.pathname.startsWith('/products/')) {
+  //     console.log("This is a product page.");
+  //     alert("Product Page);
+  //   }
+  // });`);
+
+  res.send(`
+function checkProductsPage() {
+  const currentPath = window.location.pathname;
+
+  if (currentPath.includes('/products')) {
+    console.log("You're on the products page!");
+
+    alert("Welcome to the Products Page!");
   }
-});`);
+}
+
+checkProductsPage();
+`)
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
